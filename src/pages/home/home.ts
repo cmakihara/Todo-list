a92b6e8fa52c32eea3ada386390fc9f28bf105c9
cmakihara <clmakihara@gmail.com>
2018-11-24 13:05:36 -0200

    Commit inicial.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,40 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  todos: string[] = [];
+  todo: string;
+  novo = 0;
+  index: number;
 
   constructor(public navCtrl: NavController) {
 
   }
 
+  add() {
+      this.todos.push(this.todo);
+      this.todo = "";
+      this.novo = 0;
+  }
+
+  delete(item) {
+      var index = this.todos.indexOf(item, 0);
+      if (index > -1) {
+          this.todos.splice(index, 1);
+      }
+  }
+
+  editar(item) {
+    this.todo = item;
+    this.novo = 1;
+    this.index = this.todos.indexOf(item, 0);
+    console.log(this.index);
+  }
+  modificar() {
+    if (this.index > -1) {
+      this.todos.splice(this.index, 1);
+      this.todos.push(this.todo);
+      this.novo = 0;
+      this.todo = "";
+    }
+  }
 }
